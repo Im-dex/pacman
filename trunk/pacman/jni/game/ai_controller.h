495e5ffb69b854b86f5b46f2eee71cf4a50589de
Anton Tsukanov <Im-dex@users.noreply.github.com>
2012-10-09 13:38:04 +0400

    ghosts can go from tunnel now, added frightened state support

--- trunk/pacman/jni/game/ai_controller.h
+++ trunk/pacman/jni/game/ai_controller.h
@@ -6,10 +6,12 @@
 #include <cassert>
 
 #include "base.h"
+#include "game_typedefs.h"
 #include "actor_controller.h"
 
 namespace Pacman {
 
+class IDrawable;
 class Actor;
 class Ghost;
 class GameLoader;
@@ -48,13 +50,31 @@ public:
 
     void Update(const uint64_t dt);
 
-    std::shared_ptr<Actor> GetActor(const size_t index) const;
+    std::shared_ptr<Actor> GetActor(const GhostId ghostId) const;
+
+    CellIndex GetScatterTarget(const GhostId ghostid) const;
+
+    void EnableFrightenedState();
+
+    void DisableFrightenedState();
 
     virtual void OnDirectionChanged(const MoveDirection newDirection);
 
     virtual void OnTargetAchieved();
 
 private:
+
+    enum class SelectDirectionMethod
+    {
+        Best,
+        Random
+    };
+
+    enum class SelectTargetMethod
+    {
+        OwnBehavior,
+        Scatter
+    };
 
     typedef std::array<std::shared_ptr<Ghost>, kGhostsCount> GhostsArray; 
 
@@ -68,16 +88,21 @@ private:
 
     void FindWayOnFrightenedState();
 
+    void FindWay(const SelectDirectionMethod directionMethod, const SelectTargetMethod targetMethod);
+
     MoveDirection SelectBestDirection(const CellIndex& currentCell, const CellIndex& targetCell,
                                       const MoveDirection backDirection) const;
+
+    MoveDirection SelectRandomDirection(const CellIndex& currentCell, const MoveDirection backDirection) const;
 
     CellIndex FindMoveTarget(const CellIndex& currentCell, const MoveDirection direction);
 
     void SetupScheduler();
 
-    const AIInfo mAIInfo;
-    GhostsArray  mGhosts;
-    size_t       mCurrentGhost;
+    const AIInfo               mAIInfo;
+    GhostsArray                mGhosts;
+    size_t                     mCurrentGhost;
+    std::shared_ptr<IDrawable> mFrightenedDrawable;
 };
 
 } // Pacman namespace
